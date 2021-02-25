bbb8d78ad3b202924466fc2d8876c7e29711ed4c
Marwan Subhi <marwan.subhi4@gmail.com>
2021-02-24 22:53:51 -0500

    Created Management Table Feature

--- src/test/java/com/libraryCT/pages/DashboardPage.java
+++ src/test/java/com/libraryCT/pages/DashboardPage.java
@@ -1,16 +1,41 @@
 package com.libraryCT.pages;
 
+import com.libraryCT.utilities.BrowserUtils;
+import com.libraryCT.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
-public class DashboardPage extends BasedPage{
+import java.util.List;
 
-    @FindBy(xpath ="//span[.='Dashboard']")
+public class DashboardPage extends BasedPage {
+
+    @FindBy(xpath = "//span[.='Dashboard']")
     public WebElement dashboardModule;
 
     @FindBy(xpath = "//div//h2[@id='user_count']")
     public WebElement usersCount;
 
+    @FindBy(xpath = "//*[@href='#users']")
+    private WebElement usersLink;
+
+    @FindBy(how = How.XPATH, using = "//*[@id='tbl_users']/thead/tr[1]/th")
+    private List<WebElement> headerNames;
+
+    public void clickUsersLink() {
+        this.usersLink.click();
+    }
+
+    public DashboardPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public boolean areNamesMatch(List<String> expectedNames) {
+        List<String> actualNames = BrowserUtils.getElementsText(this.headerNames);
+        if (actualNames.equals(expectedNames)) return true;
+        else return false;
+    }
 
 
 }
---
+++ src/test/java/com/libraryCT/step_definitions/ManagementTable_StepDefinitions.java
@@ -0,0 +1,47 @@
+package com.libraryCT.step_definitions;
+
+import com.libraryCT.pages.DashboardPage;
+import com.libraryCT.pages.LoginPage;
+import com.libraryCT.utilities.BrowserUtils;
+import com.libraryCT.utilities.ConfigurationReader;
+import com.libraryCT.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class ManagementTable_StepDefinitions {
+
+    LoginPage loginPage = new LoginPage();
+    DashboardPage dashboardPage = new DashboardPage();
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+
+    @Given("the user login as a librarian and be on the home page")
+    public void the_user_login_as_a_librarian_and_be_on_the_home_page() {
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        String username = ConfigurationReader.getProperty("usernameL");
+        String password = ConfigurationReader.getProperty("passwordL");
+        loginPage.loginAsLibrarian(username,password);
+
+    }
+
+    @When("the user click users module")
+    public void the_user_click_users_module() {
+        wait.until(ExpectedConditions.urlContains("dashboard"));
+        // Write code here that turns the phrase above into concrete actions
+        dashboardPage.clickUsersLink();
+
+    }
+
+
+    @Then("user should see the following")
+    public void user_should_see_the_following(List<String> dataTable) {
+
+        Assert.assertTrue( dashboardPage.areNamesMatch(dataTable));
+    }
+}
